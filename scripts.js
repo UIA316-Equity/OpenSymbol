7c5edf5aefae4338f97a2b14bf9de11b247051c7
Suraj Nanavare <surajsnanavare@gmail.com>
2022-11-20 10:53:31 +0000

    OnHover  image fixes

--- scripts.js
+++ scripts.js
@@ -1,44 +1,73 @@
+const SYMBOL_CONTAINER_ID = "symbolContainerId";
+
+function getSymbolUrlForWord(word) {
+	/**
+	 * Generate symbol url from the word
+	 */
+	let symbolUrl = `https://firebasestorage.googleapis.com/v0/b/uia-316-entity.appspot.com/o/symbols%2F${word}.webp?alt=media`;
+	return symbolUrl;
+}
+
+function showSymbolOnHover(word, top, left) {
+	/**
+	 * Generate symbol container and respective DOM elements
+	 * and render them.
+	 */
+	const symbolUrl = getSymbolUrlForWord(word);
+
+	// Image tag to show image
+	let imageTag = document.createElement('img');
+	imageTag.style.minWidth = '70px';
+	imageTag.src = symbolUrl;
+	imageTag.onerror = function () {
+		// Don't show image if not available
+		this.style.display = 'none';
+	}
+
+	// Label for currently hover word
+	let imageWord = document.createElement('p');
+	imageWord.innerText = word;
+
+	// Div to show symbol
+	let symbolContainer = document.createElement("div");
+	symbolContainer.className = "symbolContainer";
+	symbolContainer.id = SYMBOL_CONTAINER_ID;
+	symbolContainer.style.top = `${top}px`;
+	symbolContainer.style.left = `${left}px`;
+	symbolContainer.appendChild(imageTag);
+	symbolContainer.appendChild(imageWord);
+	$("body").after(symbolContainer);
+}
 
 window.onload = function highlightLineOnHover() {
-
-	var array = [];
-
-    var elements = document.body.querySelectorAll('h1, h2, h3, h4, h5, h6, p, pre, span, article, section, blockquote, ol, ul, li, strong, b, em')
-
-	// var words = $("p").text().split(" ");
-	// $("p").empty();
-	// $.each(words, function(i, v) {
-    // $("p").append($("<span>").text(v));
-	// });	
+	var elements = document.body.querySelectorAll(
+		"h1, h2, h3, h4, h5, h6, p, pre, span, article, section, blockquote, ol, ul, li, strong, b, em"
+	);
 
 	// Splits text into lines, words, characters (default)
-	const splitType = new SplitType(elements, { types: 'words', wordClass: 'highlightLineOnHover' } );
-	//console.log(splitType.words);
+	const splitType = new SplitType(elements, {
+		types: "words",
+		wordClass: "highlightLineOnHover",
+	});
 
-	// $(".highlightLineOnHover").mouseover(function(){
-	// 	$(this).select();
-	//    });
+	// Show symbol on hover
+	$(document).mouseover(function (e) {
+		const target = e.target;
+		$(target)
+			.contents()
+			.each(function (index, elem) {
+				if (elem.nodeType === 3 && $.trim(elem.nodeValue).length) {
+					let word = $.trim(elem.nodeValue).toLowerCase();
+					const left = target.offsetLeft + target.offsetWidth;
+					const top = target.offsetTop + target.offsetHeight;
+					showSymbolOnHover(word, top, left);
+					return false;
+				}
+			});
+	});
 
-	   $(document).mouseover(function(e) {
-		$(e.target).contents().each(function(index, elem) {
-			if( elem.nodeType === 3 && $.trim(elem.nodeValue).length ) {
-				let t = $.trim(elem.nodeValue)
-				let k = t.toLowerCase();
-
-				let urlv = "https://firebasestorage.googleapis.com/v0/b/uia-316-entity.appspot.com/o/symbols%2" + k + ".webp?alt=media"
-
-				
-				console.log(k);
-
-				
-				document.getElementsByClassName("highlightLineOnHover")[0].innerHTML = "<img src= https://firebasestorage.googleapis.com/v0/b/uia-316-entity.appspot.com/o/symbols%2F" + k + ".webp?alt=media>";
-				return false;
-			}
-		});
-
-		// document.addEventListener('mouseover', (event) => {
-			
-		// });
-
-});
-}
+	// Hide symbol once mouse moved to another symbol
+	$(document).mouseout(function (e) {
+		$(`#${SYMBOL_CONTAINER_ID}`).remove()
+	});
+};
